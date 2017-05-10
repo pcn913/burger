7c473bece72cfcf24ab5bcb813a31e1caaa0a674
Nannapaneni <pnannapa@nmh.org>
2017-05-10 13:45:42 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+insert into burgers
+(
+	burger_name, devoured, created_at
+)
+values
+(
+	'mcchicken', true, now()
+)
+
+;
+insert into burgers
+(
+	burger_name, devoured, created_at
+)
+values
+(
+	'mcfish', true, now()
+)
+
+;
+insert into burgers
+(
+	burger_name, devoured, created_at
+)
+values
+(
+	'mclamb', true, now()
+)
